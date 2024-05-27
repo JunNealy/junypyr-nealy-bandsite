78bffcdcae7cc05e7e88ef8f2cbe1823ddd51e14
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-05-26 22:01:49 -0400

    final styling pass for shows/bios at mobi/tab/desk breakpoints

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,14 +1,10 @@
 let showList = [
   {
-    date: 'Mon Spet 09 2024',
+    date: 'Mon Sept 09 2024',
     venue: 'Ronald Lane',
     location: 'San Francisco, CA',
   },
-  {
-    date: 'Mon Spet 09 2024',
-    venue: 'Ronald Lane',
-    location: 'San Francisco, CA',
-  },
+
   {
     date: 'Tue Sept 17 2024 ',
     venue: 'Pier 3 East ',
