e989febb138c51e652bb5e681dbc3f67b34cc51e
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-06-03 19:31:38 -0400

    wokring on date conversion class, currently implementing padstart to simplify functions

--- scripts/unix-timestamp-convert.js
+++ scripts/unix-timestamp-convert.js
@@ -1,27 +1,51 @@
-function convert(timeStamp) {
-  let monthsAbbrev = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
-  let convertedDate = '';
-  let fullDate = new Date(timeStamp);
-  let month = fullDate.getMonth() + 1;
-  let date = fullDate.getDate();
-  let day = fullDate.getDate();
-  let year = fullDate.toLocaleString('en-US', { year: 'numeric' });
-  convertedDate = `${day}  ${month}/${date}/${year}`;
-  console.log(convertedDate);
-  return convertedDate;
+class ConvertTimeCode {
+  constructor(timeStamp) {
+    this.timeStamp = timeStamp;
+    this.monthsAbbrev = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sept',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+  }
+
+  twoDigitMonth(date) {
+    let month = date.getMonth() + 1;
+    month = month.toString();
+    month = month.padStart(2, '0');
+  }
+
+  twoDigitDate(convertDate) {
+    let date = convertDate.getDate();
+    if (date.length === 1) {
+      date = date.padStart(2, '0');
+      return date;
+    } else {
+      return date;
+    }
+  }
+
+  toDsMsDDYY(timeStamp) {
+    let fullDate = new Date(timeStamp);
+    let monthString = this.twoDigitMonth(fullDate);
+    let dayOfWeek = fullDate.getDay();
+    let date = this.twoDigitDate(fullDate);
+    let year = fullDate.getFullYear();
+
+    return `${dayOfWeek} ${monthString} ${date} ${year}`;
+  }
 }
 
-export default convert;
+const concertDate = new ConvertTimeCode('1717456804');
+
+console.log(concertDate.toDsMsDDYY());
+
+export default ConvertTimeCode;
